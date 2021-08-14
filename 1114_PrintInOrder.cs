0efdc88aa8a5601523795c203a05527236e41600
Alex778ukrnet <alex778@ukr.net>
2021-08-14 16:13:05 +0300

    start point

--- 1114_PrintInOrder.cs
+++ 1114_PrintInOrder.cs
@@ -1,24 +1,69 @@
-public class Foo {
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
-    public Foo() {
-        
+namespace foobar
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+            var n = 20;
+            var f1 = new FooBar(n);
+            //new Task(f1.Foo({ i => Console.Write("foo") }));
+            Action act = new Action(MyPrintFoo);
+            //new Task(f1.Foo(() => Console.Write("foo")));
+            //f1.Foo(MyPrintFoo);
+
+            var tsk1 = Task.Factory.StartNew(()=>f1.Foo(MyPrintFoo));
+            var tsk2 = Task.Factory.StartNew(() => f1.Bar(MyPrintBar));
+
+            Thread.Sleep(2000);
+            Console.WriteLine("end");
+        }
+
+        internal static void MyPrintFoo()
+		{
+			Console.Write("foo{0}", Thread.CurrentThread.ManagedThreadId);
+		}
+        internal static void MyPrintBar()
+        {
+            Console.Write("bar{0}", Thread.CurrentThread.ManagedThreadId);
+        }
     }
 
-    public void First(Action printFirst) {
-        
-        // printFirst() outputs "first". Do not change or remove this line.
-        printFirst();
-    }
 
-    public void Second(Action printSecond) {
-        
-        // printSecond() outputs "second". Do not change or remove this line.
-        printSecond();
-    }
+    public class FooBar
+    {
+        private int n;
 
-    public void Third(Action printThird) {
-        
-        // printThird() outputs "third". Do not change or remove this line.
-        printThird();
+        public FooBar(int n)
+        {
+            this.n = n;
+        }
+
+        public void Foo(Action printFoo)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+
+                // printFoo() outputs "foo". Do not change or remove this line.
+                printFoo();
+            }
+        }
+
+        public void Bar(Action printBar)
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+
+                // printBar() outputs "bar". Do not change or remove this line.
+                printBar();
+            }
+        }
     }
 }
